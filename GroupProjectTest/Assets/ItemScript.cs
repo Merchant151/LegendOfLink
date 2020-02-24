7e445c8419e646ee88e1cd3a826f6502b202ec15
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2020-02-24 13:28:14 -0500

    added Invintory work to repo

---
+++ GroupProjectTest/Assets/ItemScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour
+{
+    public int ItemID;
+    public Color itemColor;
+    SpriteRenderer myRen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRen = gameObject.GetComponent<SpriteRenderer>();
+        float r = (float)Random.Range(0, 255);
+        //Debug.Log("float R = " + r);
+        myRen.color = new Color(r/255, (float)Random.Range(0, 255) / 255, (float)Random.Range(0, 255) / 255);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+/*
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //Debug.Log("Hey I bumped into something");
+        if (collision.tag == "Item")
+        {
+            if (Input.GetKey("x"))
+            {
+                Debug.Log("DEstroyed");
+                Destroy(collision);
+            }
+        }
+    }*/
+}
